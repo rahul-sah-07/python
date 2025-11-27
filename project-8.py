d16910b912035cfe483d5ebec394a3cf20ee22fd
Rahul Kumar Sah <s7703838@gmail.com>
2025-11-27 12:05:33 +0530

    Create project-8.py

---
+++ project-8.py
@@ -0,0 +1,17 @@
+import pyttsx3
+text = input("Enter the name of the song you wmnat to play:- ")
+
+speaker = pyttsx3.init()
+speaker.say({text})
+speaker.runAndWait()
+
+#2nd code
+
+import pyttsx3
+
+engine = pyttsx3.init()
+
+text = "hi i am rahul kuma"
+engine.say(text)
+
+engine.runAndWait()
