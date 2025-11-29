ea115386f6dff04e85c0a352a0e43608a8d82d65
Rahul Kumar Sah <s7703838@gmail.com>
2025-11-29 14:17:57 +0530

    Add file transfer server functionality
    Implement a basic file transfer server using sockets.

---
+++ project-10.py
@@ -0,0 +1,27 @@
+# File-Transfer-App
+
+import socket
+
+HOST = ""   
+PORT = 5001
+
+server = socket.socket()
+server.bind((HOST, PORT))
+server.listen(1)
+print("Waiting for sender to connect...")
+
+conn, addr = server.accept()
+print(f" Connected with: {addr}")
+
+filename = conn.recv(1024).decode()
+with open(filename, "wb") as file:
+    print(f"Receiving file: {filename}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        file.write(data)
+
+print("File received successfully!")
+conn.close()
+server.close()
